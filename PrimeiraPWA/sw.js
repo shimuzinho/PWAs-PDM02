ef5df25009e17438c59fb2b8145a9e35a4b21f7c
shimuzinho <pedro.moraes2@estudante.ifms.edu.br>
2025-08-29 17:22:38 -0400

    Update main.js

--- PrimeiraPWA/sw.js
+++ PrimeiraPWA/sw.js
@@ -3,12 +3,16 @@ let filesToCache = ["/", "/index.html", "/css/style.css", "/js/main.js"];
 
 self.addEventListener("install", (e) => {
     e.waitUntil(
-        cache.open(cacheName).then(cache => cache.addAll(filesToCache))
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        })
     );
 });
 
-self.addEventListener("fetch", (e) =>{
+self.addEventListener("fetch", (e) => {
     e.respondWith(
-        caches.match(e.request).then(response => response || fetch(e.request))
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
     );
 });
